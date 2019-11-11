96769ff6008890640d58a620f3d8d7dd20bcee39
Hans Foster <fosterhans@gmail.com>
2019-11-11 09:33:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,10 +3,16 @@ class Person
   
   attr_accessor :name, :birthday
   
-  def initialize(name:, birthday:)
-    @name = name 
-    @birthday = birthday
-    
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+  def name 
+    @name
+  end
+  
+  def birthday
+    @birthday
   end
   
     
